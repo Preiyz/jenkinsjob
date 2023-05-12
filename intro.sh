ff88e679cfb38135d655bbe01c84248ca004d1e9
Ubuntu <ubuntu@ip-172-31-5-91.ec2.internal>
2023-05-12 01:09:16 +0000

    upgrade to script

--- intro.sh
+++ intro.sh
@@ -2,3 +2,14 @@
 
 
 echo "Hello there! I hope you are having a great day."
+echo
+echo
+echo "Below is a list of the last ten Users in this server"
+echo
+echo
+cat /etc/passwd | awk -F :'{print $1}' | tail -10
+echo
+echo
+whoami = user
+echo $user
+
