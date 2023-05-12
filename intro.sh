4b67ee2de27eebdd688082908dbfcb29a46bad67
Ubuntu <ubuntu@ip-172-31-5-91.ec2.internal>
2023-05-12 01:21:30 +0000

    Additions to the script. V1.23 to 2.11

--- intro.sh
+++ intro.sh
@@ -4,12 +4,23 @@
 echo "Hello there! I hope you are having a great day."
 echo
 echo
+whoami
+echo
+echo
 echo "Below is a list of the last ten Users in this server"
 echo
 echo
-cat /etc/passwd | awk -F :'{print $1}' | tail -10
+cat /etc/passwd | awk -F : '{print $1}' | tail -10
 echo
 echo
-whoami = user
-echo $user
+date
+echo
+echo
+cal -3
+echo
+echo
+lsblk
+echo
+echo
+echo "Job Finished"
 
